320b318c2e7dd75d50feab91f9d6643c61091bcf
JPLangua41 <jplagunaf@udistrital.edu.co>
2026-02-25 16:36:27 -0500

    Tabla Multiplicar

--- src/main/java/Workshop.java
+++ src/main/java/Workshop.java
@@ -19,9 +19,11 @@ public class Workshop {
 
     // Método que retorna la tabla de multiplicar de un número
     public int[] tablaMultiplicar(int numero, int limite) {
-        // TODO: Implementar el método para retornar la tabla de multiplicar del número dado.
-        // Ejemplo: Si numero = 2 y limite = 5, el resultado debería ser [2, 4, 6, 8, 10].
-        return new int[0];
+        int tabla [limite];
+		for (int i=1; i<=limite; i++){
+			tabla[i]= numero*i;
+		}
+	return tabla[];
     }
 
     // Método que calcula el factorial de un número entero
